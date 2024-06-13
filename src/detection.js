346e794f7def2deb59970cbd0e77bb5641b53998
Idle <651462+reonZ@users.noreply.github.com>
2024-06-14 01:00:58 +0200

    disable conditional token display for now

--- src/detection.js
+++ src/detection.js
@@ -8,61 +8,42 @@ export function detectionModeTestVisibility(visionSource, mode, config = {}) {
     return config.tests.some((test) => this._testPoint(visionSource, mode, config.object, test));
 }
 
-export function basicSightCanDetect(visionSource, target, config) {
-    if (target instanceof PlaceableObject && target.document.hidden) return false;
-    if (!(target instanceof Token)) return true;
+export function visionCanDetect(wrapped, visionSource, target, config) {
+    // const canDetect = wrapped(visionSource, target);
+    // if (canDetect === false) return false;
 
-    const origin = visionSource.object;
-    const originDocument = origin.document;
-    if (
-        originDocument instanceof TokenDocument &&
-        originDocument.hasStatusEffect(CONFIG.specialStatusEffects.BLIND)
-    )
-        return false;
+    // const origin = visionSource.object;
+    // const reachedThreshold = reachesThreshold(origin, target, VISIBILITY_VALUES.hidden, config);
 
-    if (!(origin instanceof Token)) {
-        return (
-            !target.document?.hasStatusEffect(CONFIG.specialStatusEffects.INVISIBLE) &&
-            !target.actor?.hasCondition("hidden", "undetected", "unnoticed")
-        );
-    }
-
-    return !reachesThreshold(origin, target, VISIBILITY_VALUES.hidden, config);
+    return false;
+    // return !reachedThreshold;
 }
 
-export function hearingCanDetect(visionSource, target, config) {
-    if (target.document.hidden || !(target instanceof Token) || !target.actor?.emitsSound)
-        return false;
-    if (!game.settings.get("pf2e", "automation.rulesBasedVision")) return true;
+export function hearingCanDetect(wrapped, visionSource, target, config) {
+    // const canDetect = wrapped(visionSource, target);
+    // if (canDetect === false) return false;
 
-    const origin = visionSource.object;
-    if (origin.actor?.hasCondition("deafened")) return false;
+    // const origin = visionSource.object;
+    // const reachedThreshold = reachesThreshold(origin, target, VISIBILITY_VALUES.undetected, config);
 
-    if (!(origin instanceof Token)) {
-        return !target.actor?.hasCondition("undetected", "unnoticed");
-    }
-
-    return !reachesThreshold(origin, target, VISIBILITY_VALUES.undetected, config);
+    return true;
+    // return !reachedThreshold;
 }
 
 export function feelTremorCanDetect(visionSource, target, config) {
-    if (
-        target.document.hidden ||
-        !(target instanceof Token) ||
-        target.document.elevation > canvas.primary.background.elevation ||
-        target.actor?.isOfType("loot")
-    )
-        return false;
+    // const canDetect = wrapped(visionSource, target);
+    // if (canDetect === false) return false;
 
-    const origin = visionSource.object;
-    if (!(origin instanceof Token)) {
-        return !target.actor?.hasCondition("undetected", "unnoticed");
-    }
+    // const origin = visionSource.object;
+    // const reachedThreshold = reachesThreshold(origin, target, VISIBILITY_VALUES.undetected, config);
 
-    return !reachesThreshold(origin, target, VISIBILITY_VALUES.undetected, config);
+    return false;
+    // return !reachedThreshold;
 }
 
 function reachesThreshold(origin, target, threshold, config = {}) {
+    if (!origin.actor || !target.actor) return false;
+
     if (!config.visibility) {
         const perception = perceptionRules(origin, target);
         config.visibility = getVisibility(target, origin, { perception, affects: "target" });
--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import { renderChatMessage } from "./chat.js";
 import { checkRoll, renderCheckModifiersDialog } from "./check.js";
 import { renderCombatTracker, renderCombatTrackerConfig } from "./combat.js";
 import {
-    basicSightCanDetect,
+    visionCanDetect,
     detectionModeTestVisibility,
     feelTremorCanDetect,
     hearingCanDetect,
@@ -26,22 +26,17 @@ import {
     pasteToken,
     preCreateToken,
     renderTokenHUD,
-    rulesBasedVision,
     updateToken,
 } from "./token.js";
 
 const CHECK_ROLL = "game.pf2e.Check.roll";
 
-const RULES_BASED_VISION = "CONFIG.Token.documentClass.prototype.rulesBasedVision";
-
 const HIGHLIGHT_TEMPLATE_GRID = "CONFIG.MeasuredTemplate.objectClass.prototype.highlightGrid";
 
-const GET_ROLL_CONTEXT = "CONFIG.Actor.documentClass.prototype.getRollContext";
-
 const DETECTION_MODE_TEST_VISIBILITY = "DetectionMode.prototype.testVisibility";
-const BASIC_SIGHT_CAN_DETECT = "CONFIG.Canvas.detectionModes.basicSight._canDetect";
+const VISION_CAN_DETECT = "CONFIG.Canvas.detectionModes.basicSight._canDetect";
 const HEARING_CAN_DETECT = "CONFIG.Canvas.detectionModes.hearing._canDetect";
-const FEEL_TREMOR_CAN_DETECT = "CONFIG.Canvas.detectionModes.feelTremor._canDetect";
+const TREMOR_CAN_DETECT = "CONFIG.Canvas.detectionModes.feelTremor._canDetect";
 
 Hooks.once("init", () => {
     registerSettings();
@@ -52,19 +47,15 @@ Hooks.once("init", () => {
 
     libWrapper.register(MODULE_ID, HIGHLIGHT_TEMPLATE_GRID, highlightTemplateGrid, "OVERRIDE");
 
-    libWrapper.register(MODULE_ID, RULES_BASED_VISION, rulesBasedVision, "OVERRIDE");
-
-    // libWrapper.register(MODULE_ID, GET_ROLL_CONTEXT, getRollContext, 'OVERRIDE')
-
-    libWrapper.register(
-        MODULE_ID,
-        DETECTION_MODE_TEST_VISIBILITY,
-        detectionModeTestVisibility,
-        "OVERRIDE"
-    );
-    libWrapper.register(MODULE_ID, BASIC_SIGHT_CAN_DETECT, basicSightCanDetect, "OVERRIDE");
-    libWrapper.register(MODULE_ID, HEARING_CAN_DETECT, hearingCanDetect, "OVERRIDE");
-    libWrapper.register(MODULE_ID, FEEL_TREMOR_CAN_DETECT, feelTremorCanDetect, "OVERRIDE");
+    // libWrapper.register(
+    //     MODULE_ID,
+    //     DETECTION_MODE_TEST_VISIBILITY,
+    //     detectionModeTestVisibility,
+    //     "OVERRIDE"
+    // );
+    // libWrapper.register(MODULE_ID, VISION_CAN_DETECT, visionCanDetect, "MIXED");
+    // libWrapper.register(MODULE_ID, HEARING_CAN_DETECT, hearingCanDetect, "MIXED");
+    // libWrapper.register(MODULE_ID, TREMOR_CAN_DETECT, feelTremorCanDetect, "MIXED");
 
     const isGM =
         game.data.users.find((x) => x._id === game.data.userId).role >= CONST.USER_ROLES.GAMEMASTER;
